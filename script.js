b6c65d775e1337e06e8abb1fe82abc0aa6b25bb6
vaicumebanana <vaicumebanana@gmail.com>
2025-06-24 13:28:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ async function getBotResponse(userInput) {
                 ],
                 model: "meta-llama/llama-4-scout-17b-16e-instruct",
                 temperature: 1.77,
-                max_completion_tokens: 8192,
+                max_tokens: 8192,
                 top_p: 1,
                 stream: true,
                 stop: null
